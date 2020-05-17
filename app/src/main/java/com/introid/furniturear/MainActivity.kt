bf22880a2b7a3ff872210f49ce95e0c5af1086e2
prakashRanjan19 <prakash.r19500@gmail.com>
2020-05-17 22:37:51 +0530

    initial

---
+++ app/src/main/java/com/introid/furniturear/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.introid.furniturear
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupBottomSheet()
+    }
+
+    private fun setupBottomSheet(){
+        val bottomSheetBehavior = BottomSheetBehavior.from(bottomsheet)
+
+        bottomSheetBehavior.peekHeight= TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            50f,
+            resources.displayMetrics
+        ).toInt()
+
+        bottomSheetBehavior.addBottomSheetCallback(object :
+        BottomSheetBehavior.BottomSheetCallback(){
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                bottomSheet.bringToFront()
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {}
+        })
+    }
+}
