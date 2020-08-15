baf9a33fef127fd181c72b340792939b7378b88b
prakashRanjan19 <prakash.r19500@gmail.com>
2020-08-15 19:30:06 +0530

    Final

--- app/src/main/java/com/introid/furniturear/MainActivity.kt
+++ app/src/main/java/com/introid/furniturear/MainActivity.kt
@@ -1,19 +1,101 @@
 package com.introid.furniturear
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.TypedValue
 import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.lifecycle.Observer
+
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.ar.core.Anchor
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.Node
+import com.google.ar.sceneform.collision.Box
+import com.google.ar.sceneform.math.Quaternion
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.ViewRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.CompletableFuture
+
+private const val BOTTOM_SHEET_PEEK_HEIGHT = 50f
+private const val DOUBLE_TAP_TOLERANCE_MS = 1000L
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var arFragment: ArFragment
+
+
+    private val models = mutableListOf(
+        Model(R.drawable.chair, "Chair", R.raw.chair),
+        Model(R.drawable.oven, "Oven", R.raw.oven),
+        Model(R.drawable.piano, "Piano", R.raw.piano),
+        Model(R.drawable.table, "Table", R.raw.table)
+    )
+
+    lateinit var selectedModel: Model
+    private val viewNodes = mutableListOf<Node>()
+    private lateinit var photoSaver: PhotoSaver
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        arFragment = fragment as ArFragment
         setupBottomSheet()
+        setupRecyclerView()
+        setupDoubleTapArPlaneListener()
+        setupFab()
+
+        photoSaver = PhotoSaver(this)
+
+        getCurrentScene().addOnUpdateListener {
+            rotateViewNodesTowardsUser()
+        }
+
+    }
+
+    private fun setupFab() {
+        fab.setOnClickListener {
+            photoSaver.takePhoto(arFragment.arSceneView)
+        }
+    }
+
+    private fun setupRecyclerView() {
+        rvModels.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        rvModels.adapter = ModelAdapter(models).apply {
+            selectedModel.observe(this@MainActivity, Observer {
+                this@MainActivity.selectedModel = it
+                val newTitle = "Models (${it.title})"
+                tvModel.text = newTitle
+            })
+        }
+    }
+
+    // double tap to show model in plane
+
+    private fun setupDoubleTapArPlaneListener() {
+        var firstTapTime = 0L
+        arFragment.setOnTapArPlaneListener { hitResult, _, _ ->
+            if(firstTapTime == 0L) {
+                firstTapTime = System.currentTimeMillis()
+            } else if(System.currentTimeMillis() - firstTapTime < DOUBLE_TAP_TOLERANCE_MS) {
+                firstTapTime = 0L
+                loadModel { modelRenderable, viewRenderable ->
+                    addNodeToScene(hitResult.createAnchor(), modelRenderable, viewRenderable)
+                }
+            } else {
+                firstTapTime = System.currentTimeMillis()
+            }
+
+        }
     }
 
     private fun setupBottomSheet(){
@@ -21,7 +103,7 @@ class MainActivity : AppCompatActivity() {
 
         bottomSheetBehavior.peekHeight= TypedValue.applyDimension(
             TypedValue.COMPLEX_UNIT_DIP,
-            50f,
+            BOTTOM_SHEET_PEEK_HEIGHT,
             resources.displayMetrics
         ).toInt()
 
@@ -33,5 +115,78 @@ class MainActivity : AppCompatActivity() {
 
             override fun onStateChanged(bottomSheet: View, newState: Int) {}
         })
+    }
+    private fun createDeleteButton(): Button {
+        return Button(this).apply {
+            text = "Delete"
+            setBackgroundColor(Color.RED)
+            setTextColor(Color.WHITE)
+        }
+    }
+
+
+    private fun getCurrentScene() = arFragment.arSceneView.scene
+
+    private fun rotateViewNodesTowardsUser() {
+        for (node in viewNodes) {
+            node.renderable?.let {
+                val camPos = getCurrentScene().camera.worldPosition
+                val viewNodePos = node.worldPosition
+                val dir = Vector3.subtract(camPos, viewNodePos)
+                node.worldRotation = Quaternion.lookRotation(dir, Vector3.up())
+            }
+        }
+    }
+
+    // functions to add new node
+
+    private fun addNodeToScene(
+        anchor: Anchor,
+        modelRenderable: ModelRenderable,
+        viewRenderable: ViewRenderable
+    ) {
+        val anchorNode = AnchorNode(anchor)
+        val modelNode = TransformableNode(arFragment.transformationSystem).apply {
+            renderable = modelRenderable
+            setParent(anchorNode)
+            getCurrentScene().addChild(anchorNode)
+            select()
+        }
+        val viewNode = Node().apply {
+            renderable = null
+            setParent(modelNode)
+            val box = modelNode.renderable?.collisionShape as Box
+            localPosition = Vector3(0f, box.size.y, 0f)
+            (viewRenderable.view as Button).setOnClickListener {
+                getCurrentScene().removeChild(anchorNode)
+                viewNodes.remove(this)
+            }
+        }
+        modelNode.setOnTapListener { _, _ ->
+            if(!modelNode.isTransforming) {
+                if(viewNode.renderable == null) {
+                    viewNode.renderable = viewRenderable
+                } else {
+                    viewNode.renderable = null
+                }
+            }
+        }
+    }
+
+    private fun loadModel(callback: (ModelRenderable, ViewRenderable) -> Unit) {
+        val modelRenderable = ModelRenderable.builder()
+            .setSource(this, selectedModel.modelResourceId)
+            .build()
+        val viewRenderable = ViewRenderable.builder()
+            .setView(this, createDeleteButton())
+            .build()
+        CompletableFuture.allOf(modelRenderable, viewRenderable)
+            .thenAccept {
+                callback(modelRenderable.get(), viewRenderable.get())
+            }
+            .exceptionally {
+                Toast.makeText(this, "Error loading model: $it", Toast.LENGTH_LONG).show()
+                null
+            }
     }
 }
